dd62b3d7a9fa69aa1ff4ae4fdb8204dd2e877211
Sudharsan-011 <146462805+Sudharsan-011@users.noreply.github.com>
2025-10-30 20:14:18 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Api-wiz-JSONtree/",
+  base: "/",
   plugins: [tailwindcss()],
 });
